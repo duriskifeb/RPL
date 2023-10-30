c7061d2f5bc2d1e43668501026e69a9acc26472c
rizky_feb.9 <145902400+duriskifeb@users.noreply.github.com>
2023-10-30 19:02:30 +0700

    nyolong belah ketupat

--- src/semester1/Tugas6/Main.java
+++ src/semester1/Tugas6/Main.java
@@ -1,7 +1,43 @@
 package semester1.Tugas6;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }    
+import java.util.Scanner;
+
+class Main{
+    public static void main(String args[]){
+        
+        Scanner input = new Scanner(System.in);
+
+        int a;
+
+        System.out.println("MEMBUAT POLA BELAH KETUPAT");
+        System.out.println("==============================");
+        
+        System.out.print("Masukkan ukuran: ");
+        a = input.nextInt();
+
+        System.out.println("==============================");
+        
+        for(int i = 1; i <= a; i++){
+            for(int k = a; k >= i; k--){
+                System.out.print(" ");
+            }
+
+            for(int j = 1; j <= i; j++){
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+
+        for(int i = 1; i <= a; i++){
+            for(int j = 0; j <= i; j++){
+                System.out.print(" ");
+            }
+
+            for(int k = a - 1; k >= i; k--){
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+        input.close();
+    }
 }
